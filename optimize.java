dc408160609995af67649822b18844e0598c09de
Brandon Chao <bchao1@stuy.edu>
2019-02-25 14:50:11 -0500

    skeleton of optimization

---
+++ optimize.java
@@ -0,0 +1,16 @@
+public class optimize{
+  private int[][] possibles;
+
+  public optimize(int rows, int cols) {
+    
+  }
+
+  public int[] sortList(){
+
+  }
+
+  public void update(int row, int col){
+
+  }
+
+}
