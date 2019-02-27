7b625edf95bc40d32ad181dbc41c20f2c28b876c
Brandon Chao <bchao1@stuy.edu>
2019-02-26 23:38:19 -0500

    optimize creates a board with compelte total moves availiable

--- optimize.java
+++ optimize.java
@@ -2,15 +2,44 @@ public class optimize{
   private int[][] possibles;
 
   public optimize(int rows, int cols) {
-    
+    possibles = new int[rows][cols];
+    for (int r = 0; r < possibles.length; r++) {
+      for (int c = 0; c < possibles[r].length; c++) {
+        possibles[r][c] = possibleMoves(r,c);
+      }
+    }
   }
 
   public int[] sortList(){
-
+    int[] g = {1,2,1,-2,-1,2,-1,-2,2,1,2,-1,-2,1,-2,-1};
+    return g;
   }
 
   public void update(int row, int col){
 
   }
 
+  private int possibleMoves(int row, int col) {
+    int totalMoves = 0;
+    int[] g = {1,2,1,-2,-1,2,-1,-2,2,1,2,-1,-2,1,-2,-1};
+    for (int x = 0; x < g.length; x+=2 ) {
+      if ((row + g[x] > -1 && row + g[x] < possibles.length) && (col + g[x+1] > -1 && col + g[x+1] < possibles[0].length)) {
+        totalMoves++;
+      }
+    }
+    return totalMoves;
+  }
+
+  public String toString(){
+    String me = "";
+    for (int r = 0; r < possibles.length; r++) {
+      for (int c = 0; c < possibles[r].length; c++) {
+        me += possibles[r][c];
+        if (c == possibles.length-1) {
+          me+= '\n';
+        }
+      }
+    }
+    return me;
+  }
 }
